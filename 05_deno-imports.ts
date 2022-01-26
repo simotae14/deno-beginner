e3b43fa9f85bb499d2b07623804aad0132120778
Simone <startae14@gmail.com>
2022-01-26 18:11:44 +0100

    03: example Es modules and import external ts files

---
+++ 05_deno-imports.ts
@@ -0,0 +1,10 @@
+import "https://deno.land/std@0.122.0/examples/welcome.ts"
+import "https://deno.land/std@0.122.0/examples/chat/server.ts"
+
+// run with
+// deno run 05_deno-imports.ts
+// or
+// deno run 05_deno-imports.js
+
+// to permit open door 8080
+// deno run --allow-net 05_deno-imports.ts
