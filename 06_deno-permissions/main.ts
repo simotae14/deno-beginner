3b8a643626c0286663a60ba955100cdf79d293b5
Simone <startae14@gmail.com>
2022-01-27 15:49:17 +0100

    04: enable Permissions with flags

---
+++ 06_deno-permissions/main.ts
@@ -0,0 +1,7 @@
+console.log("Hello", Deno.env.get("USER"));
+// to enable permission
+// deno run --allow-env 06_deno-permissions/main.ts
+// or
+// deno run --allow-all 06_deno-permissions/main.ts
+// or the shortcut
+// deno run -A 06_deno-permissions/main.ts
