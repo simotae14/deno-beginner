d8eb3adfc83262c16f2dd190b05a88cc8f074ff0
Simone <startae14@gmail.com>
2022-01-27 16:04:09 +0100

    05: enable permissions with the command deno install

--- 06_deno-permissions/deno-example/main.ts
+++ 06_deno-permissions/deno-example/main.ts
@@ -5,3 +5,8 @@ console.log("Hello", Deno.env.get("USER"));
 // deno run --allow-all 06_deno-permissions/main.ts
 // or the shortcut
 // deno run -A 06_deno-permissions/main.ts
+
+// second option
+// deno install --allow-env deno-example/main.ts
+// and then run
+// deno-example
